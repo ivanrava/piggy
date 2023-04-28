5c59710a0ecbd08b7aaa6fd779cf16a312a51ffb
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-04-28 23:27:47 +0200

    Starts SPA login with a frontend stub

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,10 +1,51 @@
 <template>
     <h1>Login</h1>
+    <form action="javascript:void(0)" method="post">
+        <div v-if="Object.keys(validationErrors).length > 0">
+            <ul class="mb-0">
+                <li v-for="(value, key) in validationErrors" :key="key">{{ value[0] }}</li>
+            </ul>
+        </div>
+
+        <input v-model="auth.email" name="email">
+        <input v-model="auth.password" name="password">
+        <button type="submit" @click="login">
+            Login
+        </button>
+    </form>
 </template>
 
 <script>
 export default {
-    name: "Login"
+    name: "Login",
+    data() {
+        return {
+            auth: {
+                email: '',
+                password: ''
+            },
+            validationErrors: {},
+            processing: false
+        }
+    },
+    methods: {
+        async login() {
+            this.processing = true
+            await axios.get('/sanctum/csrf-cookie')
+            await axios.post('/login', this.auth).then(({data}) => {
+                console.log(data)
+            }).catch(({response}) => {
+                if (response.status === 422) {
+                    this.validationErrors = response.data.errors
+                } else {
+                    this.validationErrors = {}
+                    alert(response.data.message)
+                }
+            }).finally(() => {
+                this.processing = false
+            })
+        }
+    }
 }
 </script>
 
