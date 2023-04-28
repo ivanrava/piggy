dac2da7d6b3be096d92a3cb7ee3b04cf4d5bc6fb
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-04-28 13:05:10 +0200

    Updates default migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    private const HASH_MAX_LENGTH = 100;
+
     /**
      * Run the migrations.
      */
@@ -14,9 +16,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', self::HASH_MAX_LENGTH);
             $table->rememberToken();
             $table->timestamps();
         });
