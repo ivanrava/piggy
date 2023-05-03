023860afbf5093383420cd006d175bd6647c883b
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-03 13:54:53 +0200

    Uses custom form inputs

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,16 +1,21 @@
 <template>
-    <h1>Login</h1>
-    <form @submit.prevent="login(form)" class="flex-col">
-        <input v-model="form.email" name="email" placeholder="E-mail">
-        <input v-model="form.password" name="password" placeholder="Password">
-        <submit-button>Login</submit-button>
-    </form>
+    <main class="w-1/2 flex flex-col justify-center align-middle text-center">
+        <h1>Login</h1>
+        <form @submit.prevent="login(form)" class="flex flex-col">
+            <form-input v-model="form.email"
+                        name="email" placeholder="E-mail" type="email" />
+            <form-input v-model="form.password"
+                        name="password" placeholder="Password" type="password" />
+            <submit-button>Login</submit-button>
+        </form>
+    </main>
 </template>
 
 <script setup lang="ts">
 import {ref} from "vue";
 import {useAuth} from "../composables/useAuth";
 import SubmitButton from "../components/SubmitButton.vue";
+import FormInput from "../components/FormInput.vue";
 
 const form = ref({
     email: '',
