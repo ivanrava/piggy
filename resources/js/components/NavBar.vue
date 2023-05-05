1689c7b5537f27a76be76bce6810fe4e2fadad88
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-05 17:51:58 +0200

    Makes brand logo clickable

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="h-16 bg-slate-200 flex justify-between items-center">
-    <brand-logo class="h-full p-3" />
+    <brand-logo class="h-full p-3 cursor-pointer" @click="router.push('home')" />
     <router-link class="p-6 text-lg text-slate-950 tracking-widest font-light uppercase" to="/logout">Logout</router-link>
   </nav>
 </template>
@@ -8,10 +8,13 @@
 <script setup>
 import {defineComponent} from "vue";
 import BrandLogo from "../components/BrandLogo.vue";
+import {useRouter} from "vue-router";
 
 defineComponent({
   components: {BrandLogo}
 })
+
+const router = useRouter()
 </script>
 
 <style scoped>
