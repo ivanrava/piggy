63ccc0584bc53ef52f9500760551ba38bc7e12b8
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-07 15:57:05 +0200

    Adds whole DB seeder logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Account;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AccountTypeSeeder::class
         ]);
+        User::factory()->count(3)->create()->each(function (User $user) {
+            $user->accounts()->saveMany(Account::factory()->count(10)->make());
+        });
     }
 }
