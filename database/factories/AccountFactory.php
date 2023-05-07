2cd70bc87bd7230a9fe07e3efcc2d9984ae0e5ad
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-07 15:56:49 +0200

    Define AccountFactory

--- database/factories/AccountFactory.php
+++ database/factories/AccountFactory.php
@@ -16,8 +16,24 @@ class AccountFactory extends Factory
      */
     public function definition(): array
     {
+        $startDate = fake()->dateTime;
         return [
-            //
+            'user_id' => fake()->numberBetween(0, 100),
+            'account_type_id' => fake()->numberBetween(1, 4),
+            'initial_balance' => fake()->randomFloat(
+                2,
+                0,
+                1000
+            ),
+            'name' => fake()->creditCardType,
+            'icon' => fake()->randomElement(['mdi:abacus', 'mdi:access-point',
+                'solar:adhesive-plaster-2-linear', 'carbon:accumulation-snow']),
+            'color' => substr(fake()->hexColor, 1),
+            'opening' => $startDate,
+            'closing' => fake()->optional()->dateTimeBetween(
+                $startDate->add(\DateInterval::createFromDateString('1 day'))
+            ),
+            'description' => fake()->text(500)
         ];
     }
 }
