91a10d3cdae4693ec137547c5c01dc16754b5594
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-07 11:39:13 +0200

    Adds index and show methods to accounts

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -5,15 +5,16 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreAccountRequest;
 use App\Http\Requests\UpdateAccountRequest;
 use App\Models\Account;
+use Illuminate\Http\JsonResponse;
 
 class AccountController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        return response()->json(Account::all());
     }
 
     /**
@@ -27,9 +28,9 @@ class AccountController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Account $account)
+    public function show(Account $account): JsonResponse
     {
-        //
+        return response()->json($account);
     }
 
     /**
