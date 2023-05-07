860a83fad2a5ff476c96e2099393e9ca91f4d227
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-07 11:21:49 +0200

    Adds Accounts backend boilerplate

---
+++ database/migrations/2023_04_28_132828_create_accounts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private const TOTAL_DIGITS = 10;
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            //$table->foreignId('user_id')->constrained();
+            $table->foreignId('account_type_id')->constrained();
+            $table->decimal('initial_balance', self::TOTAL_DIGITS, 2);
+            $table->string('name');
+            $table->string('icon');
+            // TODO: maybe a rigid length?
+            $table->string('color');
+            $table->date('opening');
+            $table->date('closing')->nullable();
+            // TODO: add a maximum length
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
