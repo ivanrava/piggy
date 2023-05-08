c0325e1321b6e4b75741291a6010c00420431fd6
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-08 12:59:56 +0200

    Adds transactions boilerplate

---
+++ app/Http/Controllers/TransferController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTransferRequest;
+use App\Http\Requests\UpdateTransferRequest;
+use App\Models\Transfer;
+
+class TransferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTransferRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Transfer $transfer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTransferRequest $request, Transfer $transfer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Transfer $transfer)
+    {
+        //
+    }
+}
