1209bec5193d1a1fdd6d4744b76e80af528ce48b
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-08 12:57:53 +0200

    Adds categories boilerplate

---
+++ database/migrations/2023_05_08_105343_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('parent_category_id')->nullable();
+            $table->string('name', 100);
+            $table->enum('type', ['out', 'in']);
+            $table->string('icon', 200);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
