19ec574ad3b41c103d10cf1c4691441ba026c842
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-08 12:58:45 +0200

    Adds beneficiaries boilerplate

---
+++ app/Http/Controllers/BeneficiaryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBeneficiaryRequest;
+use App\Http\Requests\UpdateBeneficiaryRequest;
+use App\Models\Beneficiary;
+
+class BeneficiaryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBeneficiaryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Beneficiary $beneficiary)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBeneficiaryRequest $request, Beneficiary $beneficiary)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Beneficiary $beneficiary)
+    {
+        //
+    }
+}
