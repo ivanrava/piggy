8457b1ed258195a917bc5cf48be2bf394441d5ae
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-08 20:12:21 +0200

    Adds seeder stub

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Account;
+use App\Models\Beneficiary;
+use App\Models\Category;
+use App\Models\Transaction;
+use App\Models\Transfer;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -18,7 +22,15 @@ class DatabaseSeeder extends Seeder
             AccountTypeSeeder::class
         ]);
         User::factory()->count(3)->create()->each(function (User $user) {
-            $user->accounts()->saveMany(Account::factory()->count(25)->make());
+            $user->beneficiaries()->saveMany(Beneficiary::factory()->count(20)->make());
+            $user->categories()->saveMany(Category::factory()->count(20)->make());
+            $user->accounts()->saveMany(Account::factory()->count(25)->make()->each(
+                function (Account $account) {
+                    $account->transactions()->saveMany(Transaction::factory()->count(50)->make());
+                    $account->in_transfers()->saveMany(Transfer::factory()->count(2)->make());
+                    $account->out_transfers()->saveMany(Transfer::factory()->count(2)->make());
+                }
+            ));
         });
     }
 }
