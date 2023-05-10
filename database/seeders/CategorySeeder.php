37a3a6fb2c4c35281bd7ad9c1575b6dc8a58840d
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-10 09:26:20 +0200

    Refines the category seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -20,10 +20,27 @@ class CategorySeeder extends Seeder
 
     public static function seed(User $user): void
     {
-        $user->categories()->saveMany(Category::factory()->count(5)->make());
-        $user->categories()->each(function (Category $category) {
-            $category->parent_category_id = fake()->randomElement([$category->id % 20 - 1 == 0 ? null : $category->id - 1, null]);
-            $category->save();
+        $user->categories()->saveMany(Category::factory()->count(10)->make());
+        $user->categories()->each(
+            fn (Category $parent) => self::maybeAddRandomChildren($parent)
+        );
+        $user->categories()->each(function (Category $parent) {
+            $parent->children()->each(
+                fn (Category $child) => self::maybeAddRandomChildren($child)
+            );
         });
+    }
+
+    private static function maybeAddRandomChildren(Category $parent): void
+    {
+        // Do not add any child at random
+        if (fake()->boolean)
+            return;
+
+        // Adds a random number of children of coherent user and type
+        $children = Category::factory()->count(
+            fake()->randomElement([1, 2])
+        )->make(['user_id' => $parent->user_id, 'type' => $parent->type]);
+        $parent->children()->saveMany($children);
     }
 }
