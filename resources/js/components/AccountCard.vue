bbca675f09bd181eb2d8bbbb83a3f21bf0081a94
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-23 14:48:15 +0200

    Adds brightness-based text color

--- resources/js/components/AccountCard.vue
+++ resources/js/components/AccountCard.vue
@@ -1,14 +1,14 @@
 <template>
   <router-link
-    class="flex items-center text-slate-50 drop-shadow-xl cursor-pointer unstyled"
-    :class="small ? 'rounded-md px-2' : 'rounded-lg p-2'"
+    class="flex items-center shadow-lg cursor-pointer unstyled"
+    :class="[small ? 'rounded-md px-2' : 'rounded-lg p-2', textColor]"
     :style="{backgroundColor: `#${account.color}`}"
     :to="'/accounts/'+account.id"
   >
     <Icon :icon="account.icon" :class="small ? 'text-2xl' : 'text-4xl'" />
     <div class="flex flex-col ml-2">
       <b :class="{'leading-4 text-xs': small}">{{ account.name }}</b>
-      <small class="text-stone-100 font-light" :class="{'text-[8px] leading-[0.8]': small}">{{ account.type }}</small>
+      <small class="font-light" :class="{'text-[8px] leading-[0.8]': small}">{{ account.type }}</small>
     </div>
   </router-link>
 </template>
@@ -17,15 +17,23 @@
 import {Icon} from "@iconify/vue";
 import {Account} from '../composables/interfaces';
 import {useRouter} from "vue-router";
+import {computed} from "vue";
 const router = useRouter()
 
-withDefaults(defineProps<{
+const props = withDefaults(defineProps<{
   account: Account,
   small: boolean
 }>(), {
   small: false
 })
 
+const textColor = computed(() => {
+  const brightness = Math.round(((parseInt(props.account.color.slice(0,2), 16) * 299) +
+    (parseInt(props.account.color.slice(2,4), 16) * 587) +
+    (parseInt(props.account.color.slice(4,6), 16) * 114)) / 1000);
+
+  return brightness > 180 ? 'text-slate-950' : 'text-slate-50';
+})
 </script>
 
 <style scoped>
