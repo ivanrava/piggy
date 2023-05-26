38186b2c76ae9d50f3a805515a00a92b0e3d8f12
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-26 10:36:07 +0200

    Makes clickable beneficiary cards

--- resources/js/components/BeneficiaryCard.vue
+++ resources/js/components/BeneficiaryCard.vue
@@ -8,7 +8,10 @@ defineProps<{
 </script>
 
 <template>
-  <div class="w-56 shadow-sm rounded-lg flex flex-row items-center bg-slate-100 pr-2">
+  <router-link
+    class="w-56 shadow-sm rounded-lg flex flex-row items-center bg-slate-100 pr-2 unstyled cursor-pointer hover:shadow-md transition-all"
+    :to="`/beneficiaries/${beneficiary.id}`"
+  >
     <beneficiary-image :beneficiary="beneficiary" />
     <div class="flex flex-col min-w-0">
       <span class="block text-left text-xs font-medium truncate overflow-hidden">
@@ -18,7 +21,7 @@ defineProps<{
         First added {{ beneficiary.created_at }}
       </span>
     </div>
-  </div>
+  </router-link>
 </template>
 
 <style scoped>
