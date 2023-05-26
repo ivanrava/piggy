76e4945830bcf6cb45f8dffda94c452a8f3cfb3e
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-26 14:24:54 +0200

    Adds skeletons

--- resources/js/pages/dashboard/Accounts.vue
+++ resources/js/pages/dashboard/Accounts.vue
@@ -1,33 +1,45 @@
 <template>
-  <!--  <table class="table">-->
-  <!--    <thead>-->
-  <!--      <tr class="text-xl border-b-2">-->
-  <!--        <th class="text-left">-->
-  <!--          Account-->
-  <!--        </th>-->
-  <!--        <th class="text-right">-->
-  <!--          Balance-->
-  <!--        </th>-->
-  <!--      </tr>-->
-  <!--    </thead>-->
-  <!--    <tbody>-->
-  <!--      <account-row-->
-  <!--        v-for="account in accounts"-->
-  <!--        :key="account.name"-->
-  <!--        :account="account"-->
-  <!--      />-->
-  <!--    </tbody>-->
-  <!--  </table>-->
   <div class="flex flex-col justify-between h-full">
     <section>
       <h1>Accounts</h1>
-      <div class="flex flex-wrap gap-4">
-        <account-card
-          v-for="account in accounts"
-          :key="account"
-          :account="account"
-        />
-      </div>
+      <Transition name="fade" mode="out-in">
+        <div role="status" v-if="isLoading">
+          <div class="flex flex-wrap items-center w-full gap-4">
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-32"></div>
+            <div class="animate-pulse h-14 bg-gray-400/70 rounded-lg w-24"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-40"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-40"></div>
+            <div class="animate-pulse h-14 bg-gray-400/70 rounded-lg w-40"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-24"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-32"></div>
+            <div class="animate-pulse h-14 bg-gray-400/70 rounded-lg w-80"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-32"></div>
+            <div class="animate-pulse h-14 bg-gray-400/70 rounded-lg w-32"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-32"></div>
+            <div class="animate-pulse h-14 bg-gray-400/70 rounded-lg w-24"></div>
+            <div class="animate-pulse h-14 bg-gray-400/70 rounded-lg w-32"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-24"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-36"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-36"></div>
+            <div class="animate-pulse h-14 bg-gray-400/70 rounded-lg w-80"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-42"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-32"></div>
+            <div class="animate-pulse h-14 bg-gray-400/70 rounded-lg w-24"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-36"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-32"></div>
+            <div class="animate-pulse h-14 bg-gray-400/70 rounded-lg w-80"></div>
+            <div class="animate-pulse h-14 bg-gray-400 rounded-lg w-32"></div>
+          </div>
+          <span class="sr-only">Loading...</span>
+        </div>
+        <div class="flex flex-wrap gap-4" v-else>
+          <account-card
+            v-for="account in accounts"
+            :key="account"
+            :account="account"
+          />
+        </div>
+      </Transition>
     </section>
     <form-account />
   </div>
@@ -41,7 +53,7 @@ import AccountCard from "../../components/AccountCard.vue";
 import FormAccount from "../../components/form/crud/AccountForm.vue";
 
 const accounts = ref([]);
-const isLoading = ref(false);
+const isLoading = ref(true);
 
 onMounted(async () => {
   isLoading.value = true;
@@ -53,10 +65,27 @@ onMounted(async () => {
   }
   isLoading.value = false;
 })
+
+const randomColorWidth = () => {
+  const colors = ['bg-gray-400', 'bg-gray-400/60']
+  const widths = ['w-32', 'w-36', 'w-24', 'w-28', 'w-40', 'w-52']
+  const w = widths[Math.floor(Math.random() * widths.length)]
+  const c = colors[Math.floor(Math.random() * colors.length)]
+  return `${c} ${w}`;
+}
 </script>
 
 <style scoped>
 th {
   @apply p-2;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.4s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
--- resources/js/pages/dashboard/Beneficiaries.vue
+++ resources/js/pages/dashboard/Beneficiaries.vue
@@ -5,7 +5,7 @@ import BeneficiaryForm from "../../components/form/crud/BeneficiaryForm.vue";
 import BeneficiaryCard from "../../components/BeneficiaryCard.vue";
 
 const beneficiaries = ref([]);
-const isLoading = ref(false);
+const isLoading = ref(true);
 
 onMounted(async () => {
   isLoading.value = true;
@@ -22,16 +22,49 @@ onMounted(async () => {
 <template>
   <h1>Beneficiaries</h1>
   <div class="flex flex-col justify-between">
-    <section class="flex flex-wrap gap-4 justify-start">
-      <beneficiary-card
-        v-for="beneficiary in beneficiaries"
-        :beneficiary="beneficiary"
-      />
-    </section>
+    <Transition name="fade" mode="out-in">
+      <section v-if="isLoading" class="flex flex-wrap items-center w-full gap-4">
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-52"></div>
+        <div class="animate-pulse h-16 bg-gray-400/70 rounded-lg w-56"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-52"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-52"></div>
+        <div class="animate-pulse h-16 bg-gray-400/70 rounded-lg w-48"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-56"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-52"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-60"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-52"></div>
+        <div class="animate-pulse h-16 bg-gray-400/70 rounded-lg w-48"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-56"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-56"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-52"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-60"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-52"></div>
+        <div class="animate-pulse h-16 bg-gray-400/70 rounded-lg w-56"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-60"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-60"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-52"></div>
+        <div class="animate-pulse h-16 bg-gray-400/70 rounded-lg w-48"></div>
+        <div class="animate-pulse h-16 bg-gray-400 rounded-lg w-56"></div>
+      </section>
+      <section class="flex flex-wrap gap-4 justify-start pb-4" v-else>
+        <beneficiary-card
+          v-for="beneficiary in beneficiaries"
+          :beneficiary="beneficiary"
+        />
+      </section>
+    </Transition>
     <beneficiary-form />
   </div>
 </template>
 
 <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.4s ease;
+}
 
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
