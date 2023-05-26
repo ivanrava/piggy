1621b38b5c91edd5cf34cc06ded5dabbe8c19dcb
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-05-26 16:43:59 +0200

    Blurs searchbar on escape

--- resources/js/components/nav/NavBar.vue
+++ resources/js/components/nav/NavBar.vue
@@ -21,6 +21,7 @@
         class="w-full bg-pink-100/40 hover:bg-stone-500/10 outline-none focus:outline-none focus:ring-4 focus:ring-offset-0 focus:bg-gray-50/10 transition-all p-2 rounded-lg ring-1 ring-gray-50/30 ring-offset-1 ring-offset-pink-50 cursor-pointer text-xs"
         @focus="focused = true"
         @blur="focused = false"
+        @keydown.esc="$event.target.blur()"
       />
       <span
         class="absolute left-48 top-1 text-slate-900 transition-all duration-300"
