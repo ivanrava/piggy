4456777e3c2d6dcb63c8dc294a19b2e5e575930d
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-06-03 17:49:28 +0200

    Fixes for null fields

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\MakeReportRequest;
 use App\Http\Resources\TransactionResource;
 use App\Models\Transaction;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ReportController extends Controller
@@ -12,8 +13,9 @@ class ReportController extends Controller
     public function report(MakeReportRequest $request): AnonymousResourceCollection
     {
         return TransactionResource::collection(
-            Transaction::whereBetween('date', [$request->from, $request->to])
-                ->orderBy('date')
+            Transaction::orderBy('date')
+                ->when($request->has('from'), fn (Builder $query) => $query->where('date', '>=', $request->from))
+                ->when($request->has('to'), fn (Builder $query) => $query->where('date', '<=', $request->to))
                 ->get()
         );
     }
