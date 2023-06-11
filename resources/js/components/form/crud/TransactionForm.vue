6ee8cb9c9423651b6a5940b239b8220cc253b2b2
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-06-11 18:27:26 +0200

    Factors out an in/out selector component

--- resources/js/components/form/crud/TransactionForm.vue
+++ resources/js/components/form/crud/TransactionForm.vue
@@ -9,6 +9,7 @@ import FormTextarea from "../inputs/FormTextarea.vue";
 import BeneficiaryImage from "../../BeneficiaryImage.vue";
 import DecimalInput from "../inputs/DecimalInput.vue";
 import BeneficiaryForm from "./BeneficiaryForm.vue";
+import InOutInput from "../inputs/InOutInput.vue";
 import CategoryForm from "./CategoryForm.vue";
 
 const showForm = ref(false);
@@ -212,22 +213,7 @@ watchEffect(() => form.value.category = computedCategories.value.find(c => c.id 
             v-else-if="transactionStoreUrl === '/transfers'"
             class="w-full flex flex-row gap-4 justify-center items-center"
           >
-            <label
-              class="inline-flex items-center cursor-pointer mt-6"
-              @click="isOut = !isOut"
-            >
-              <span class="mx-2 text-sm font-serif text-gray-500">To</span>
-              <div
-                class="w-12 h-6 rounded-full relative transition-all duration-300"
-                :class="isOut ? 'bg-red-800/50' : 'bg-green-800/50'"
-              >
-                <div
-                  class="absolute h-6 w-6 bg-white rounded-full border-[1px] transition-all"
-                  :class="isOut ? 'left-0 border-red-900/30' : 'left-1/2 border-green-900/30'"
-                />
-              </div>
-              <span class="mx-2 text-sm font-serif text-gray-500">From</span>
-            </label>
+            <in-out-input v-model="isOut" />
             <select-input
               v-slot="{option}"
               v-model="selectedAccountId"
---
+++ resources/js/components/form/inputs/InOutInput.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {computed} from "vue";
+
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+  },
+})
+const emit = defineEmits(['update:modelValue'])
+
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  }
+})
+</script>
+
+<template>
+  <label
+    class="inline-flex items-center cursor-pointer mt-6"
+    @click="value = !value"
+  >
+    <span class="mx-2 text-sm font-serif text-gray-500">To</span>
+    <div
+      class="w-12 h-6 rounded-full relative transition-all duration-300"
+      :class="value ? 'bg-red-800/50' : 'bg-green-800/50'"
+    >
+      <div
+        class="absolute h-6 w-6 bg-white rounded-full border-[1px] transition-all"
+        :class="value ? 'left-0 border-red-900/30' : 'left-1/2 border-green-900/30'"
+      />
+    </div>
+    <span class="mx-2 text-sm font-serif text-gray-500">From</span>
+  </label>
+</template>
+
+<style scoped>
+
+</style>
