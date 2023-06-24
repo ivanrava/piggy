81b0917a8b51c31524a43d1af77642569c36fa0d
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-06-24 14:44:20 +0200

    Highlights selected category

--- resources/js/components/CategoryList.vue
+++ resources/js/components/CategoryList.vue
@@ -23,25 +23,24 @@
 <script setup lang="ts">
 import {Category} from '../composables/interfaces';
 import {Icon} from "@iconify/vue";
-import {ref} from "vue";
 import {useCategoriesStore} from "../composables/useCategoriesStore";
 
 defineProps<{
   categories: Array<Category>
 }>();
 
-const selected = ref(0)
+const store = useCategoriesStore();
 
 const classes = function(c: Category) {
-  if (selected.value === c.id) {
-    return c.type === 'out' ? 'bg-red-500 text-stone-50' : 'bg-green-500 text-stone-50'
+  if (store.selectedCategory.id === c.id) {
+    return c.type === 'out' ? 'bg-red-500/70 text-stone-50 font-semibold' : 'bg-green-500/70 text-stone-50 font-semibold'
   } else {
     return c.type === 'out' ? 'hover:bg-red-100/50 hover:!text-red-950' : 'hover:bg-green-100/50 hover:!text-green-950'
   }
 }
 
 const selectCategory = (clickedCategory: Category) => {
-  useCategoriesStore().selectCategory(clickedCategory);
+  store.selectCategory(clickedCategory);
 }
 </script>
 
