d07df84d73775658baf02f76912ef5341496a080
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-06-25 13:47:21 +0200

    Adds component for input radio

---
+++ resources/js/components/form/inputs/RadioInput.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import {computed} from "vue";
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+  },
+  label: {
+    type: String,
+  },
+  options: {
+    type: Array<{
+      id: String,
+      display: String
+    }>
+  },
+  errors: {
+    type: Array<String>,
+    default: []
+  }
+})
+const emit = defineEmits(['update:modelValue'])
+
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  }
+})
+</script>
+
+<template>
+  <div class="font-bold mt-2 text-lg mb-1">
+    {{ label }}
+  </div>
+  <div class="flex gap-4 mb-2">
+    <span
+      v-for="option in options"
+      :key="option.id"
+    >
+      <input
+        :id="option.id"
+        v-model="value"
+        type="radio"
+        :value="option.id"
+        class="mr-1"
+      >
+      <label :for="option.id">
+        {{ option.display }}
+      </label>
+    </span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
