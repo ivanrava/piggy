2ebd55b98c9b51534b820c09372dbe8e44cfea4b
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-06-29 22:09:02 +0200

    Better style and account navigation

--- resources/js/components/nav/SideBarAccountList.vue
+++ resources/js/components/nav/SideBarAccountList.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
 import {Account} from "../../composables/interfaces";
 import {Icon} from "@iconify/vue";
+import {ref} from "vue";
+import {useRouter} from "vue-router";
 
 defineProps<{
   accounts: Array<Account>
 }>();
+
+const router = useRouter();
+
+const askedForDeletion = ref(null);
+const askDelete = (account: Account) => {
+  askedForDeletion.value = null;
+  setTimeout(() => {
+    askedForDeletion.value = account.id;
+  }, 500)
+}
 </script>
 
 <template>
@@ -12,26 +24,70 @@ defineProps<{
     v-for="account in accounts"
     :key="account.id"
     class="my-0.5 w-full relative text-stone-900"
+    @click="router.push(`/accounts/${account.id}`)"
   >
-    <span class="absolute right-0 z-10 pr-2 py-1.5 cursor-pointer">
-      <Icon icon="fluent:delete-16-regular" />
-    </span>
-    <span class="absolute right-6 z-10 pr-2 py-1.5 cursor-pointer">
-      <Icon icon="radix-icons:pencil-1" />
-    </span>
-    <div
-      class="flex items-center px-2 py-1 rounded-sm hover:shadow-sm transition-all cursor-pointer"
-      :style="{backgroundColor: `#${account.color}33`}"
+    <Transition
+      mode="out-in"
+      name="slide-delete"
     >
-      <Icon
-        :icon="account.icon"
-        class="mr-1"
-      />
-      <span class="font-medium text-sm tracking-tighter">{{ account.name }}</span>
-    </div>
+      <div
+        v-if="askedForDeletion == account.id"
+        class="flex items-center px-2 py-1 rounded-sm text-red-900"
+      >
+        <span class="font-medium text-sm tracking-tighter">Really sure?</span>
+        <span
+          class="absolute right-6 z-10 px-2 py-1.5 cursor-pointer"
+          @click="askedForDeletion = null"
+        >
+          <Icon icon="carbon:undo" />
+        </span>
+        <span
+          class="absolute right-0 z-10 px-2 py-1.5 cursor-pointer"
+          @click="askDelete(account)"
+        >
+          <Icon icon="fluent:delete-16-regular" />
+        </span>
+      </div>
+      <div
+        v-else
+        class="flex items-center px-2 py-1 rounded-sm hover:shadow-sm transition-all cursor-pointer"
+        :style="{backgroundColor: `#${account.color}22`}"
+      >
+        <Icon
+          :icon="account.icon"
+          class="mr-1"
+        />
+        <span class="text-sm tracking-tighter">{{ account.name }}</span>
+        <span class="absolute right-6 z-10 px-2 py-1.5 cursor-pointer opacity-40 hover:opacity-100 transition-all">
+          <Icon icon="radix-icons:pencil-1" />
+        </span>
+        <span
+          class="absolute right-0 z-10 px-2 py-1.5 cursor-pointer opacity-40 hover:opacity-100 transition-all"
+          @click="askDelete(account)"
+        >
+          <Icon icon="fluent:delete-16-regular" />
+        </span>
+      </div>
+    </Transition>
   </div>
 </template>
 
 <style scoped>
+.slide-delete-enter-active {
+  transition: all 0.4s cubic-bezier(.62,.16,.13,1.01);
+}
 
+.slide-delete-leave-active {
+  transition: all 0.4s cubic-bezier(.62,.16,.13,1.01);
+}
+
+.slide-delete-enter-from {
+  transform: translateX(120%);
+}
+.slide-delete-enter-to {
+  opacity: 100%;
+}
+.slide-delete-leave-to {
+  opacity: 0;
+}
 </style>
