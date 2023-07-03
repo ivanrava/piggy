061f171acb87d6f99f43745e21279de50fdb2934
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-07-03 23:17:20 +0200

    Adds a bit of RWD to the searchbar

--- resources/js/components/nav/NavBar.vue
+++ resources/js/components/nav/NavBar.vue
@@ -27,24 +27,7 @@
         </ul>
       </nav>
     </div>
-    <label class="grow mx-32 relative cursor-pointer">
-      <input
-        class="w-full bg-pink-100/40 hover:bg-stone-500/10 outline-none focus:outline-none focus:ring-4 focus:ring-offset-0 focus:bg-gray-50/10 transition-all p-2 rounded-lg ring-1 ring-gray-50/30 ring-offset-1 ring-offset-pink-50 cursor-pointer text-xs"
-        @focus="focused = true"
-        @blur="focused = false"
-        @keydown.esc="$event.target.blur()"
-      >
-      <span
-        class="absolute left-48 top-1 text-slate-900 transition-all duration-300"
-        :class="focused ? 'opacity-0' : 'opacity-60'"
-      >
-        <Icon
-          icon="ic:round-search"
-          class="inline"
-        />
-        Search
-      </span>
-    </label>
+    <search-bar />
     <router-link
       class="p-6 !text-lg !text-slate-800 hover:!text-stone-600 tracking-widest font-light uppercase"
       to="/logout"
@@ -59,12 +42,12 @@ import {defineComponent, ref} from "vue";
 import BrandLogo from "./BrandLogo.vue";
 import {useRoute, useRouter} from "vue-router";
 import {Icon} from "@iconify/vue";
+import SearchBar from "./SearchBar.vue";
 
 defineComponent({
   components: {BrandLogo}
 })
 
-const focused = ref(false);
 const router = useRouter()
 
 const links = [
---
+++ resources/js/components/nav/SearchBar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import {Icon} from "@iconify/vue";
+import {ref} from "vue";
+
+const focused = ref(false);
+const query = ref('');
+</script>
+
+<template>
+  <label class="grow mx-32 relative cursor-pointer">
+    <input
+      v-model="query"
+      class="w-full bg-pink-100/40 hover:bg-stone-500/10 outline-none focus:outline-none focus:ring-4 focus:ring-offset-0 focus:bg-gray-50/10 transition-all p-2 rounded-lg ring-1 ring-gray-50/30 ring-offset-1 ring-offset-pink-50 cursor-pointer text-xs"
+      @focus="focused = true"
+      @blur="focused = false"
+      @keydown.esc="$event.target.blur()"
+    >
+    <span
+      class="absolute left-1/2 top-1 text-slate-900 transition-all duration-300 -translate-x-1/2"
+      :class="focused || query.length > 0 ? 'opacity-0' : 'opacity-60'"
+    >
+      <Icon
+        icon="ic:round-search"
+        class="inline"
+      />
+      Search
+    </span>
+  </label>
+</template>
+
+<style scoped>
+
+</style>
