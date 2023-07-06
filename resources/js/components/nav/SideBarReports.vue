6c115cea265aff9a894defcebf00a17a4a5fd6a7
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-07-06 20:22:07 +0200

    Removes beneficiary report sidebar

--- resources/js/components/nav/SideBarReports.vue
+++ resources/js/components/nav/SideBarReports.vue
@@ -7,23 +7,23 @@ const links = [
     to: '/report',
     description: 'Get a glimpse of the categories. Expenses VS Incomes'
   },
-  {
-    name: 'Beneficiaries',
-    to: '/report/beneficiaries',
-    description: 'Get a glimpse of the beneficiaries.'
-  }
+  // {
+  //   name: 'Beneficiaries',
+  //   to: '/report/beneficiaries',
+  //   description: 'Get a glimpse of the beneficiaries.'
+  // }
 ]
 </script>
 
 <template>
   <section>
     <side-bar-generic :links="links" />
-    <p class="text-sm tracking-tighter text-center text-stone-900/60 my-32">
-      Looking for more reports?
-      Did you know that most of the pages are <i>printable by default</i>?
-      <br>
-      Hit that pesky <span class="font-mono">Ctrl-P</span> and see for yourself.
-    </p>
+<!--    <p class="text-sm tracking-tighter text-center text-stone-900/60 my-32">-->
+<!--      Looking for more reports?-->
+<!--      Did you know that most of the pages are <i>printable by default</i>?-->
+<!--      <br>-->
+<!--      Hit that pesky <span class="font-mono">Ctrl-P</span> and see for yourself.-->
+<!--    </p>-->
   </section>
 </template>
 
