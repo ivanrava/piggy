215d21b4ee8e97c9dbe81e728687abab9f8df5a6
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-07-06 14:52:03 +0200

    RWD for categories index

--- resources/js/pages/dashboard/categories/Categories.vue
+++ resources/js/pages/dashboard/categories/Categories.vue
@@ -2,6 +2,7 @@
 import CategoryForm from "../../../components/form/crud/CategoryFormWrapper.vue";
 import {onMounted} from "vue";
 import {useCategoriesStore} from "../../../composables/useCategoriesStore";
+import SideBarCategories from "../../../components/nav/SideBarCategories.vue";
 
 onMounted(() => {
   useCategoriesStore().selectCategory(null)
@@ -11,20 +12,26 @@ onMounted(() => {
 <template>
   <div class="h-full flex flex-col justify-between">
     <section class="overflow-scroll">
-      <h1>
-        No category selected
-      </h1>
-      <div
-        class="flex flex-col items-center gap-8 justify-center w-full p-8 my-16"
-      >
-        <img
-          class="w-1/3"
-          src="/investing.svg"
-          alt="No data"
+      <div class="hidden md:block">
+        <h1>
+          No category selected
+        </h1>
+        <div
+          class="flex flex-col items-center gap-8 justify-center w-full p-8 my-16"
         >
-        <span class="block text-stone-600">
-          We will load data from your categories in here, please click one of the categories on the sidebar.
-        </span>
+          <img
+            class="w-1/3"
+            src="/investing.svg"
+            alt="No data"
+          >
+          <span class="block text-stone-600">
+            We will load data from your categories in here, please click one of the categories on the sidebar.
+          </span>
+        </div>
+      </div>
+      <div class="md:hidden">
+        <h1>Categories</h1>
+        <side-bar-categories />
       </div>
     </section>
     <category-form />
