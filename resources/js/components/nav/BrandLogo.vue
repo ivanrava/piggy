c212acc9fe61def73748b3e3054e27454d386223
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-07-25 18:34:56 +0200

    Fixes logo styling

--- resources/js/components/nav/BrandLogo.vue
+++ resources/js/components/nav/BrandLogo.vue
@@ -2,15 +2,15 @@
   <img
     alt="Application logo"
     src="/logo-align-left.svg"
-    class="hover:brightness-75 transition-all hidden md:block cursor-pointer h-full p-2.5 dark:invert"
-    :class="useRoute().path === '/home' ? 'brightness-50' : ''"
+    class="hover:brightness-50 transition-all hidden md:block cursor-pointer h-full p-2.5 dark:invert"
+    :class="useRoute().path === '/home' ? 'brightness-50 dark:brightness-0' : 'dark:brightness-125 dark:opacity-90'"
     @click="router.push('/home')"
   >
   <img
     alt="Application logo"
     src="/logo-icon.svg"
-    class="hover:brightness-75 transition-all block md:hidden cursor-pointer h-full p-1 dark:invert"
-    :class="useRoute().path === '/home' ? 'brightness-50' : ''"
+    class="hover:brightness-50 transition-all block md:hidden cursor-pointer h-full p-1 dark:invert"
+    :class="useRoute().path === '/home' ? 'brightness-50 dark:brightness-0' : 'dark:brightness-125 dark:opacity-90'"
     @click="router.push('/home')"
   >
 </template>
