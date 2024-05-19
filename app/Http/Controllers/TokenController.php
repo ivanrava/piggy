6b55ff4253eab4b2e31530328a7e183af96560f8
Ivan Ravasi <ivan4.ravasi@gmail.com>
2024-05-19 18:41:04 +0200

    Returns json response in TokenController

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -22,6 +22,8 @@ class TokenController extends Controller
             ]);
         }
 
-        return $user->createToken($request->device_name)->plainTextToken;
+        return response()->json([
+            'token' => $user->createToken($request->device_name)->plainTextToken
+        ]);
     }
 }
